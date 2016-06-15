4483cb5e720e05d76895f7d8959e9a5458ca1e33
sparsha dotel <sparsha dotel>
2016-06-15 10:43:24 +0545

    Comitted this directly to master

--- Basics/app/src/main/java/com/internlft/activitylogger/MainActivity.java
+++ Basics/app/src/main/java/com/internlft/activitylogger/MainActivity.java
@@ -9,27 +9,28 @@ import android.widget.Toast;
 import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
-   // UsageStats usageStats;
-    //String thisAppName = usageStats.getPackageName();
-    //String timeUsed = Long.toString(usageStats.getTotalTimeInForeground());// Shows total Usage Time
-    //String totalStats = "Name: " + thisAppName+ "Usage Time: " + timeUsed;
+    UsageStats usageStats;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    
     public void monitorApp(View view){
         boolean checkToggle = ((ToggleButton)view).isChecked();
-        if(checkToggle == true){
+        if(checkToggle){
             Toast.makeText(getBaseContext(),"Monitoring Started",Toast.LENGTH_SHORT).show();
         }
         else {
             Toast.makeText(getBaseContext(),"Monitoring Stopped",Toast.LENGTH_SHORT).show();
+            String thisAppName = usageStats.getPackageName();
+            String timeUsed = Long.toString(usageStats.getTotalTimeInForeground());// Shows total Usage Time
+            String totalStats = "Name: " + thisAppName+ "Usage Time: " + timeUsed;
 
 
 
-      //      TextView textView = (TextView)findViewById(R.id.stats);
+            //      TextView textView = (TextView)findViewById(R.id.stats);
         //    textView.setText(thisAppName);
         }
     }
