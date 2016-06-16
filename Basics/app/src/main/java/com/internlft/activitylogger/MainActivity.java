41e5598a7339ba228241fcbfe0809a2920ff2590
sparsha dotel <sparsha dotel>
2016-06-16 09:23:36 +0545

    removed unnecessary funcitons

--- Basics/app/src/main/java/com/internlft/activitylogger/MainActivity.java
+++ Basics/app/src/main/java/com/internlft/activitylogger/MainActivity.java
@@ -1,5 +1,6 @@
 package com.internlft.activitylogger;
 
+import android.app.usage.UsageStatsManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.app.usage.UsageStats;
@@ -7,17 +8,21 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
+import android.os.Parcelable;
 
 public class MainActivity extends AppCompatActivity {
     UsageStats usageStats;
+    UsageStats justBlank = new UsageStats(usageStats);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    
+
     public void monitorApp(View view){
+
         boolean checkToggle = ((ToggleButton)view).isChecked();
         if(checkToggle){
             Toast.makeText(getBaseContext(),"Monitoring Started",Toast.LENGTH_SHORT).show();
